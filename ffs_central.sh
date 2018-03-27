f2a543f90d2d884ed165c9c5c65d96b9a86836b3
Unknown <finkernagel@imt.uni-marburg.de>
2018-03-27 14:59:42 +0200

    switch user only if not ffs in the first place

--- ffs_central.sh
+++ ffs_central.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd "${0%/*}"
-exec sudo -u ffs /bin/sh - << 'EOF'
-python3 ffs_central.py 
-EOF
+cmd=$1
+#exec sudo -u ffs /bin/sh - << 'EOF'
+#python3 ffs_central.py "$@"
+#EOF
+if [ "$(whoami)" != "ffs" ]; then
+    sudo -u ffs /usr/bin/python3 ffs_central.py $@
+else
+    /usr/bin/python3 ffs_central.py $@
+fi
