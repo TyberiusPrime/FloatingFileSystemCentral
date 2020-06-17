4434cb318d78c144f6e3e010acbef0394027643e
Unknown <finkernagel@imt.uni-marburg.de>
2020-06-17 13:09:20 +0200

    fixed install script

--- scripts/ffs_central.sh
+++ scripts/ffs_central.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd "${0%/*}"
-cmd=$1
+d="${0%/*}"
 #exec sudo -u ffs /bin/sh - << 'EOF'
 #python3 ffs_central.py "$@"
 #EOF
 if [ "$(whoami)" != "ffs" ]; then
-    sudo -u ffs python3 ffs_central.py $@ 
+    sudo -u ffs $d/python3 ffs_central.py $@ 
 else
-    python3 ffs_central.py $@ 
+    $d/python3 ffs_central.py $@ 
 fi
